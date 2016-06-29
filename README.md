800b2c5c5653a401dcfd96f2315f1e5d821b0adb
Dao Lam <daolam112@gmail.com>
2016-06-29 13:28:28 -0700

    Add start, stop

--- README.md
+++ README.md
@@ -9,6 +9,33 @@
 npm install screwdriver-executor-base
 ```
 
+## Extending
+```js
+class MyExecutor extends Executor {
+    // Implement the interface
+    start(config, callback) {
+        if (config.buildId) {
+            // do stuff here...
+            return callback(null);
+        }
+
+        return process.nextTick(() => {
+            callback(new Error('Error starting executor'));
+        });
+    }
+}
+
+const executor = new MyExecutor({});
+executor.start({
+    buildId: '4b8d9b530d2e5e297b4f470d5b0a6e1310d29c5e',
+    jobId: '50dc14f719cdc2c9cb1fb0e49dd2acc4cf6189a0',
+    pipelineId: 'ccc49349d3cffbd12ea9e3d41521480b4aa5de5f',
+    container: 'node:4',
+    scmUrl: 'git@github.com:screwdriver-cd/data-model.git#master'
+}, (err) => {
+    // do something...
+});
+```
 ## Testing
 
 ```bash
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,47 @@
 'use strict';
 const assert = require('chai').assert;
+const Executor = require('../index');
 
 describe('index test', () => {
-    it('fails', () => {
-        assert.isTrue(false);
+    let instance;
+
+    beforeEach(() => {
+        instance = new Executor({ foo: 'bar' });
+    });
+
+    afterEach(() => {
+        instance = null;
+    });
+
+    it('can create a Executor base class', () => {
+        assert.instanceOf(instance, Executor);
+    });
+
+    it('has methods that need to be extended', () => {
+        assert.throws(instance.start, Error, 'not implemented');
+        assert.throws(instance.stop, Error, 'not implemented');
+    });
+
+    it('can be extended', (done) => {
+        class Foo extends Executor {
+            get(id, callback) {
+                if (id > 0) {
+                    return callback(null, { id });
+                }
+
+                return process.nextTick(() => {
+                    callback(new Error('invalid id'));
+                });
+            }
+        }
+
+        const bar = new Foo({ foo: 'bar' });
+
+        assert.instanceOf(bar, Executor);
+        bar.get(1, (err, data) => {
+            assert.isNull(err);
+            assert.equal(data.id, 1);
+            done();
+        });
     });
 });
